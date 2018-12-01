beae8b8aada991ce13eda791669f82c4be0918a8
PaulBeaudet <inof8or@gmail.com>
2018-12-01 17:40:42 -0500

    update migration to fix fields

--- slackKeyField.js
+++ slackKeyField.js
@@ -1,16 +1,10 @@
 // slackKeyField.js ~ Copyright 2018 Manchester Makerspace ~ MIT License
-ObjectID = require('mongodb').ObjectID;
-var mongo = {
-    client: require('mongodb').MongoClient,
-    connectAndDo: function(connected, failed){         // url to db and what well call this db in case we want multiple
-        mongo.client.connect(process.env.TEST_MONGODB_URI, function onConnect(error, db){
-            if(db){connected(db);} // passes database object so databasy things can happen
-            else  {failed(error);} // what to do when your reason for existence is a lie
-        });
-    }
-};
-
+// This is a migration that created the slack users collection
+var ObjectID = require('mongodb').ObjectID;
+var MongoClient = require('mongodb').MongoClient;
+var DB_NAME = process.env.DB_NAME;
 var request = require('request');
+
 var slack = {
     dir: [],
     compileDir: function(onFinish){  // recursively pagenates through request untill a member is or isn't found
@@ -31,6 +25,8 @@ var slack = {
                         email: resBody.members[i].profile.email,
                         fullname: resBody.members[i].profile.real_name_normalized.toLowerCase(),
                         altname: resBody.members[i].profile.display_name_normalized.toLowerCase(),
+                        real_name: resBody.members[i].real_name,
+                        display_name: resBody.members[i].profile.display_name,
                         name: resBody.members[i].name,
                         id: resBody.members[i].id,
                         lastname: resBody.members[i].profile.last_name ? resBody.members[i].profile.last_name.toLowerCase() : '',
@@ -45,12 +41,12 @@ var slack = {
 };
 
 check = {
-    stream: function(cursor, db, stream, onFinish){
-        cursor.nextObject(function onDoc(error, record){     // when next document in the stream is ready
+    stream: function(cursor, client, stream, onFinish){
+        cursor.next(function onDoc(error, record){     // when next document in the stream is ready
             if(error){console.log('on check: ' + error);}
             else if(record){                                      // as long as stream produces records
                 stream(record);                              // function for streaming docs into
-                check.stream(cursor, db, stream, onFinish);  // recursively move through all members in collection
+                check.stream(cursor, client, stream, onFinish);  // recursively move through all members in collection
             } else if(onFinish){onFinish();}
         });
     }
@@ -68,12 +64,13 @@ var migrate = {
     },
     updates: 0,
     misses: 0,
-    updateSlackInfo: function(db){
-        check.stream(db.collection('members').find({}), db, function onDoc(doc){ // for every member document
+    updateSlackInfo: function(client){
+        check.stream(client.db(DB_NAME).collection('members').find({}), client, function onDoc(doc){ // for every member document
             for(var i = 0; i < slack.dir.length; i++){ // check if there is a slack email that matches email (both.toLowerCase)
                 if(slack.dir[i].email){
                     if(slack.dir[i].email.toLowerCase() === doc.email.toLowerCase()){
-                        migrate.createSlackUser(db, doc, slack.dir[i]);
+                        // migrate.createSlackUser(client, doc, slack.dir[i]);
+                        migrate.updateSlackUser(client, doc, slack.dir[i]);
                         slack.dir[i].matched = true;
                         // migrate.success(slack.dir[i].fullname, 'email match');
                         return; // no need to go further
@@ -85,7 +82,8 @@ var migrate = {
             var lowerFullname = fullname.toLowerCase(); var lowerAltFullname = altFullname.toLowerCase();
             for(var x = 0; x < slack.dir.length; x++){
                 if(slack.dir[x].fullname === lowerFullname || slack.dir[x].fullname === lowerAltFullname){
-                    migrate.createSlackUser(db, doc, slack.dir[x]);
+                    // migrate.createSlackUser(client, doc, slack.dir[x]);
+                    migrate.updateSlackUser(client, doc, slack.dir[x]);
                     slack.dir[x].matched = true;
                     // migrate.success(slack.dir[x].fullname, 'fullname match');
                     return;
@@ -95,7 +93,8 @@ var migrate = {
             if(new Date().getTime() < new Date(doc.expirationTime).getTime()){ // look by last name if current in membership
                 for(var y = 0; y < slack.dir.length; y++){
                     if(slack.dir[y].lastname === doc.lastname.toLowerCase() || slack.dir[y].lastname === lowerFullname){
-                        migrate.createSlackUser(db, doc, slack.dir[y]);
+                        // migrate.createSlackUser(client, doc, slack.dir[y]);
+                        migrate.updateSlackUser(client, doc, slack.dir[y]);
                         slack.dir[y].matched = true;
                         // migrate.success(slack.dir[y].fullname, 'lastname match');
                         return;
@@ -105,47 +104,60 @@ var migrate = {
                 inGoodStanding = false;
                 for(var b = 0; b < slack.dir.length; b++){
                     if(slack.dir[b].lastname === doc.lastname.toLowerCase()){
-                        console.log(fullname + ' potentialy matches with ' + slack.dir[b].email + ' slackid:' + slack.dir[b].id);
+                        // console.log(fullname + ' potentialy matches with ' + slack.dir[b].email + ' slackid:' + slack.dir[b].id);
                     }
                 }
             }
             migrate.fail(fullname, doc.email, inGoodStanding);
         });
     },
-    createSlackUser: function(db, memberDoc, slack_user){
-        db.collection('slack_users').insertOne({
+    createSlackUser: function(client, memberDoc, slack_user){
+        client.db(DB_NAME).collection('slack_users').insertOne({
             _id: new ObjectID(),
             member_id: memberDoc._id,
             slack_email: slack_user.email,
             slack_id: slack_user.id,
-            name: slack_user.name
+            name: slack_user.display_name,
+            real_name: slack_user.real_name
         });
     },
-    duplicateEmail: function(db){
-        check.stream(db.collection('members').find({}), db, function onDoc(doc){
-            db.collection('members').update({email: memberDoc.email}, {$set: {slackEmail: memberDoc.email}});
+    updateSlackUser: function(client, memberDoc, slack_user){
+        client.db(DB_NAME).collection('slack_users').updateOne({slack_id: slack_user.id},{
+            $set: {
+                member_id: memberDoc._id,
+                slack_email: slack_user.email,
+                slack_id: slack_user.id,
+                name: slack_user.display_name,
+                real_name: slack_user.real_name
+            }
+        }, {upsert: true}, function(error, result){
+            if(error){console.log(error);}
         });
     },
-    printResults: function(db){
-        check.stream(db.collection('members').find({slackEmail: {$exists: 1, $nin: ['']}}), db, function onDoc(doc){
+    printResults: function(client){
+        check.stream(client.db(DB_NAME).collection('members').find({slackEmail: {$exists: 1, $nin: ['']}}), db, function onDoc(doc){
             console.log(doc.slackEmail + ':' + doc.firstname + ' ' + doc.lastname);
         }, function(){console.log('done printResults');});
     },
-    nonMatches: function(db){
+    nonMatches: function(client){
         for(var i=0; i < slack.dir.length; i++){
             if(!slack.dir[i].matched){
-                migrate.createSlackUser(db, {_id: null}, slack.dir[i]);
+                // migrate.createSlackUser(client, {_id: null}, slack.dir[i]);
+                migrate.updateSlackUser(client, {id: null}, slack.dir[i]);
             }
         }
     }
 };
 
 slack.compileDir(function whenCopiled(){
-    mongo.connectAndDo(function onConnect(db){
-        migrate.updateSlackInfo(db);
-        setTimeout(function(){
-            migrate.nonMatches(db);
-            setTimeout(function(){db.close();}, 14000);
-        }, 10000);
-    }, console.log);
+    MongoClient.connect(process.env.TEST_MONGODB_URI, {useNewUrlParser: true}, function onConnect(connectError, client){
+        if(connectError){console.log('connect error ' + connectError);}
+        else {
+            migrate.updateSlackInfo(client);
+            setTimeout(function(){
+                migrate.nonMatches(client);
+                setTimeout(function(){client.close();}, 14000);
+            }, 10000);
+        }
+    });
 });
